47a93518cf9f71c14fcc45356285f9c24b3d79a4
Ali <alihasan89207@gmail.com>
2022-01-16 11:41:01 +0530

    frist

---
+++ index.js
@@ -0,0 +1,16 @@
+var age=10;
+var value=20;
+var sam=age+value;
+
+//console.log(sam);
+//console.log(age);
+var name='ali hasan';
+console.log(name);
+var x=10;
+var y=25;
+var sam=x*y;
+console.log(sam);
+
+
+
+
